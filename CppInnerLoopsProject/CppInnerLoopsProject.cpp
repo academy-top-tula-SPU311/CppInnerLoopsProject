ac4de2c9c458f185df8f0c0ccf9d9c17618aa613
maxefimov <efimov.top.academy@gmail.com>
2024-01-20 12:59:48 +0300

    Добавьте файлы проекта.

---
+++ CppInnerLoopsProject/CppInnerLoopsProject.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n{ 11 };
+    
+    for (int i{}; i < n; i++)
+    {
+        for (int j{}; j < n; j++)
+            if(i == j || j == n - i - 1
+                || i == n / 2 || j == n / 2)
+                std::cout << "* ";
+            else
+                std::cout << "  ";
+        std::cout << "\n";
+    }
+
+    
+}
